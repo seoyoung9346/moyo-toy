2b78196203f06b43bb05ca6ddc411d23c9bb5867
윤서영 <seoyoung9346@gmail.com>
2022-10-14 15:59:11 +0900

    questions constant 설정 - 통신사 선택

---
+++ src/constants/questions.js
@@ -0,0 +1,23 @@
+
+
+export const questions = [
+    {
+        id: 1,
+        question: '현재 사용 중인 통신사를 선택해주세요.',
+        answers: [
+            {
+                companyID: 1,
+                content: 'LGU',
+            },
+            {
+                companyID: 2,
+                content: 'SKT',
+            },
+            {
+                companyID: 3,
+                content: 'KT',
+            }
+        ]
+
+    }
+]
